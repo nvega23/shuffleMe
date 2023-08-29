93cfce868fbb560e5f5f816dcff09eda524c7ffe
Nestor Vega <vega.nestor1@gmail.com>
2023-08-28 22:16:04 -0700

    script changed

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const people = [
-    'Cynthia <3', 'Ana', 'Paty', 'Brenda'
-  ];
+    "Cynthia <3", "Ana", "Paty", "Brenda"
+]
 
-  const assignRandomly = () => {
-    const shuffledPeople = [...people].sort(() => Math.random() - 0.5);
-    const halfLength = Math.ceil(shuffledPeople.length / 2);
-    const box1People = shuffledPeople.slice(0, halfLength);
-    const box2People = shuffledPeople.slice(halfLength);
-    
-    displayPeople(box1People, 'box1');
-    displayPeople(box2People, 'box2');
-  };
+const assignRandomly = () => {
+    const shufflePeople = [...people].sort(()=> Math.random() - 0.5);
+    const halfLength = Math.ceil(shufflePeople.length / 2);
+    const box1People = shufflePeople.slice(0, halfLength);
+    const box2People = shufflePeople.slice(halfLength);
 
-  const displayPeople = (peopleList, containerId) => {
+    displayPeople(box1People, "box1")
+    displayPeople(box2People, "box2")
+}
+
+const displayPeople = (peopleList, containerId) => {
     const container = document.getElementById(containerId);
     container.innerHTML = '';
-    
-    peopleList.forEach((person) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = person;
-      container.appendChild(listItem);
-    });
-  };
+
+    peopleList.forEach((person)=>{
+        const listItem = document.createElement('li');
+        listItem.textContent = person;
+        container.appendChild(listItem)
+    })
+}
